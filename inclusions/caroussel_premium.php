e108acfddc88a0e453fd8717fe829ef71ebce55e
Demba <dembadiax@gmail.com>
2015-07-13 00:32:22 +0000

    Some PHP

---
+++ inclusions/caroussel_premium.php
@@ -0,0 +1,74 @@
+<!--Premium Advertisement-->
+<section id="premium">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="main-heading text-center">
+                    <h2>PREMIUM ADVERTISEMENT</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="owl-carousel">
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Old Car</a></div>
+                    <a href="#" class="item-hover"><span>$199</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Expensive Car</a></div>
+                    <a href="#" class="item-hover"><span>$299</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Antique things for sale</a></div>
+                    <a href="#" class="item-hover"><span>$499</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Creative Tattos</a></div>
+                    <a href="#" class="item-hover"><span>$99</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Old Car</a></div>
+                    <a href="#" class="item-hover"><span>$199</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Old Car</a></div>
+                    <a href="#" class="item-hover"><span>$199</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Expensive Car</a></div>
+                    <a href="#" class="item-hover"><span>$299</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Antique things for sale</a></div>
+                    <a href="#" class="item-hover"><span>$499</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Creative Tattos</a></div>
+                    <a href="#" class="item-hover"><span>$99</span></a>
+                </div>
+                <div class="item">
+                    <a href="#"><img src="http://placehold.it/291x250" alt="carousel"></a>
+                    <div class="item-title"><a href="#">Old Car</a></div>
+                    <a href="#" class="item-hover"><span>$199</span></a>
+                </div>
+            </div><!--end carousel-->
+        </div>
+    </div>
+</section><!--End Premium Advertisement-->
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="advertise">
+            <a href="#"><img src="http://placehold.it/960x90" alt="ad"> </a>
+        </div>
+    </div>
+</div>
---
+++ inclusions/pied.php
@@ -0,0 +1,38 @@
+<!--Footer-->
+<section id="footer">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-sm-6">
+                    <div class="widget">
+                        <h4>A PROPOS</h4>
+                        <p>Lorem ipsum dolor sit amet sectetuer esl adipiscing elit sed diam nonummy nibhi euismod tincidunt ut <span class="col">laoreet dolore</span> amet magna aliquam erat volutpat. </p>
+                        <p>Ut wisi enim ad minim veniam quis dest nostrud exerci tation ullamcorper norme
+                            suscipit lobortis commodo consequat.</p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="widget">
+                        <h4>NEWSLETTER</h4>
+                        <p class="sign">Lorem ipsum dolor sit amet sectetuer in adipiscing elit sed diam...</p>
+                        <p class="sign">S'inscrire à notre newsletter !</p>
+                        <form method="post">
+                            <input type="email" name="email" placeholder="Votre adresse email">
+                            <input type="submit" name="submit" value="S'inscrire">
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright text-center">
+                        <p>Kogn Bi, Tous droits réservés - 2015</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+    </footer>
+</section>
+<!--End Footer-->
